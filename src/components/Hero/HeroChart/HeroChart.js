5dcca816a4c31322d83549e5632b6b11d133f7b9
Pradeep Sekar <pradeep87sekar@gmail.com>
2018-01-23 19:43:17 +1100

    Hero chart component 65% complete.

--- src/components/Hero/HeroChart/HeroChart.js
+++ src/components/Hero/HeroChart/HeroChart.js
@@ -39,34 +39,66 @@ class HeroChart extends Component {
     //   Private members
     // ---------------------------------------------
     _data = [
-        { week:  1, intv_best: 54, intv_avg: 33, intv_score: 26, swng_best:  54, swng_avg:  33, swng_score:  39 },
-        { week:  2, intv_best: 63, intv_avg: 35, intv_score: 37, swng_best: 104, swng_avg:  64, swng_score:  76 },
-        { week:  3, intv_best: 79, intv_avg: 41, intv_score: 36, swng_best: 168, swng_avg: 102, swng_score: 122 },
-        { week:  4, intv_best: 55, intv_avg: 30, intv_score: 39, swng_best: 201, swng_avg: 126, swng_score: 153 },
-        { week:  5, intv_best: 79, intv_avg: 41, intv_score: 46, swng_best: 251, swng_avg: 164, swng_score: 196 },
-        { week:  6, intv_best: 65, intv_avg: 37, intv_score: 44, swng_best: 305, swng_avg: 199, swng_score: 240 },
-        { week:  7, intv_best: 89, intv_avg: 51, intv_score: 56, swng_best: 315, swng_avg: 209, swng_score: 250 },
-        { week:  8, intv_best: 79, intv_avg: 41, intv_score: 46, swng_best: 251, swng_avg: 164, swng_score: 196 },
-        { week:  9, intv_best: 55, intv_avg: 30, intv_score: 39, swng_best: 201, swng_avg: 126, swng_score: 153 },
-        { week: 10, intv_best: 79, intv_avg: 41, intv_score: 36, swng_best: 168, swng_avg: 102, swng_score: 122 },
-        { week: 11, intv_best: 63, intv_avg: 35, intv_score: 37, swng_best: 104, swng_avg:  64, swng_score:  76 },
-        { week: 12, intv_best: 54, intv_avg: 33, intv_score: 26, swng_best:  54, swng_avg:  33, swng_score:  39 }
+        { week:  1, intv_best: 55, intv_avg: 31, intv_score: 36, swng_best:  76, swng_avg:  45, swng_score:  54 },
+        { week:  2, intv_best: 58, intv_avg: 34, intv_score: 38, swng_best:  79, swng_avg:  48, swng_score:  57 },
+        { week:  3, intv_best: 63, intv_avg: 35, intv_score: 37, swng_best: 104, swng_avg:  64, swng_score:  76 },
+        { week:  4, intv_best: 71, intv_avg: 38, intv_score: 41, swng_best: 136, swng_avg:  83, swng_score:  99 },
+        { week:  5, intv_best: 79, intv_avg: 41, intv_score: 46, swng_best: 168, swng_avg: 102, swng_score: 122 },
+        { week:  6, intv_best: 67, intv_avg: 35, intv_score: 38, swng_best: 184, swng_avg: 114, swng_score: 137 },
+        { week:  7, intv_best: 55, intv_avg: 30, intv_score: 31, swng_best: 201, swng_avg: 126, swng_score: 153 },
+        { week:  8, intv_best: 67, intv_avg: 35, intv_score: 38, swng_best: 226, swng_avg: 145, swng_score: 174 },
+        { week:  9, intv_best: 79, intv_avg: 41, intv_score: 46, swng_best: 251, swng_avg: 164, swng_score: 196 },
+        { week: 10, intv_best: 72, intv_avg: 39, intv_score: 45, swng_best: 278, swng_avg: 182, swng_score: 218 },
+        { week: 11, intv_best: 65, intv_avg: 37, intv_score: 44, swng_best: 305, swng_avg: 199, swng_score: 240 },
+        { week: 12, intv_best: 62, intv_avg: 34, intv_score: 41, swng_best: 308, swng_avg: 202, swng_score: 243 }
     ];
 
     _intvData = [ ];
     _swngData = [ ];
 
+    _el = {
+        main: null,
+        svg:  null,
+        grp:  null
+    }
+
+    _classes = {
+        main: "hchart",
+        svg:  "hchart__svg",
+        grp:  "hchart__svg__g",
+
+        bar:  "hchart__svg__bar",
+        path: "hchart__svg__path"
+    }
+
+    _modifiers = {
+        interval: "--theme-interval",
+        swing:    "--theme-swing",
+        lineb:    "--line-best",
+        linea:    "--line-avg"
+    }
+
     _width  = 0;
     _height = 0;
 
-    _margin = {
-        bar: 20,
-        top:  0, left: 20,
-        right: 0, bottom: 20
+    _windowWidth = 0;
+    _mobileWidth = {
+        small: 480,
+        deflt: 780
     };
 
-    _el  = null;
+    _margin = {
+        bar: 20,
+        top:  20, left: 0,
+        right: 0, bottom: 0
+    };
+
     _type = "";
+
+    _animation = {
+        bar:  { duration:  500, delay: 75 },
+        path: { duration: 3000, delay:  0 }
+    }
 
     // ---------------------------------------------
     //   Public members
@@ -83,124 +115,12 @@ class HeroChart extends Component {
         super(props);
 
         this._onDataChange(this._data);
-
         this._onTypeChange(this._type);
     }
 
     // ---------------------------------------------
     //   Private methods
     // ---------------------------------------------
-    _createChart = (el) => {
-        const svgElem = d3.select(el)
-                        .append("svg")
-                        .attr("class", "hchart__svg hchart__svg--theme-" + this._type);
-
-        const gElem   = svgElem.append("g")
-                        .attr("class", "hchart__svg__g");
-
-        gElem.attr("transform", "translate(" + (this._margin.left - this._margin.right) + ", 0)");
-
-        console.log("---------------");
-        console.log("gElem:",   gElem);
-        console.log("svgElem:", svgElem);
-
-        // x and y scales
-        const x = d3.scaleBand()
-                    .rangeRound([0, this._width]);
-
-        const y = d3.scaleLinear()
-                    .rangeRound([this._height, 0]);
-
-        const y1 = d3.scaleLinear()
-                    .rangeRound([this._height, 0]);
-
-        // line scales
-        const lineAvg = d3.line()
-                        .x(d => x(d.week) + x.bandwidth() / 2)
-                        .y(d => y(d.avg))
-
-                        // stop line if the value is null
-                        .defined(d => d.avg);
-
-        const lineBest = d3.line()
-                        .x(d => x(d.week) + x.bandwidth() / 2)
-                        .y(d => y(d.best))
-
-                        // stop line if the value is null
-                        .defined(d => d.best);
-
-        console.log("-------------");
-        console.log("x:",  typeof x);
-        console.log("y:",  typeof y);
-        console.log("y1:", typeof y1);
-
-        console.log("-------------------------");
-        console.log("lineAvg:",  typeof lineAvg);
-        console.log("lineBest:", typeof lineBest);
-
-        switch(this._type) {
-            case "interval": {
-                this._drawChart(el, this._intvData, x, y, y1); break; }
-            case "swing": {
-                this._drawChart(el, this._swngData, x, y, y1); break; }
-            default:
-        }
-    };
-
-    _destroyChart = () => {
-
-    };
-
-    _updateChart = () => {
-
-    };
-
-    _drawChart = (el, data, x, y, y1) => {
-        data = [...data];
-        console.log("-----------");
-        console.log("data:", data);
-
-        // add domains
-        x.domain(data.map(d => d.week));
-        y.domain([0, d3.max(data, d => d.best)]);
-        y1.domain([0, d3.max(data, d => d.score)]);
-
-        // adjust avg height
-        const rate1 = d3.max(data, d => d.score) /
-                      d3.max(data, d => d.best);
-
-        console.log("-------------");
-        console.log("rate1:", rate1);
-
-        // bar points
-        const gElem   = d3.select(el)
-                        .select(".hchart__svg__g");
-
-        const barElem = gElem.selectAll(".hchart__svg__bar")
-                        .data(data);
-
-        console.log("-------------");
-        console.log("gElem:", gElem);
-        console.log("barElem:", barElem);
-
-        barElem.enter()
-            .append("rect")
-            .attr("class", "hchart__svg__bar")
-
-            .attr("x", (d) => x(d.week))
-            .attr("y", this._height)
-
-            .transition().duration(500)
-            .delay((d, i) => (i * 75))
-
-            .attr("y", (d) => (y1(d.score)
-                            + (this._height - y1(d.score)
-                            - (this._height - y1(d.score)) * rate1)))
-
-            .attr("width", ((this._width / data.length) - this._margin.bar))
-            .attr("height", (d) => ((this._height - y1(d.score)) * rate1))
-    };
-
     _onDataChange = (data) => {
         let intvData = [ ];
         let swngData = [ ];
@@ -255,13 +175,19 @@ class HeroChart extends Component {
         }
     };
 
-    _onWindowResize = (el) => {
-        const elRect = el.getBoundingClientRect()
-        this._width  = elRect.width  - this._margin.left - this._margin.right + this._margin.bar;
+    _onWindowResize = () => {
+        this._windowWidth = window.innerWidth
+                         || document.documentElement.clientWidth
+                         || document.body.clientWidth;
+
+        const elRect = this._el.main.getBoundingClientRect()
         this._height = elRect.height - this._margin.top  - this._margin.bottom;
+        this._width  = elRect.width  - this._margin.left - this._margin.right
+                                     + (this._margin.bar / (this._windowWidth < this._mobileWidth.small ? 2 : 1));
 
         console.log("-------------------------");
         console.log("this._width:", this._width);
+        console.log("this._windowWidth:", this._windowWidth);
         console.log("this._margin.left:", this._margin.left);
         console.log("this._margin.right:", this._margin.right);
 
@@ -271,17 +197,157 @@ class HeroChart extends Component {
         console.log("this._margin.bottom:", this._margin.bottom);
     };
 
+    _createChart = () => {
+        const svgElem = d3.select(this._el.main)
+                        .append("svg");
+
+        this._el.svg = svgElem.node();
+        this._el.svg.classList.add(this._classes.svg);
+        this._el.svg.classList.add(this._classes.svg + this._modifiers[this._type]);
+
+        const grpElem = svgElem
+                        .append("g");
+
+        this._el.grp = grpElem.node();
+        this._el.grp.classList.add(this._classes.grp);
+        this._el.grp.setAttribute("transform", "translate(" + (this._margin.left - this._margin.right)  + ", "
+                                                            + (this._margin.top  - this._margin.bottom) + ")");
+
+        console.log("--------------------------");
+        console.log("this._el.grp", this._el.grp);
+        console.log("this._el.svg:", this._el.svg);
+
+        // x and y scales
+        const x = d3.scaleBand()
+                    .rangeRound([0, this._width]);
+
+        const y = d3.scaleLinear()
+                    .rangeRound([this._height, 0]);
+
+        const y1 = d3.scaleLinear()
+                    .rangeRound([this._height, 0]);
+
+        // line scales
+        const lineAvg = d3.line()
+                        .x(d => x(d.week) + x.bandwidth() / 2)
+                        .y(d => y(d.avg))
+
+                        // stop line if the value is null
+                        .defined(d => d.avg);
+
+        const lineBest = d3.line()
+                        .x(d => x(d.week) + x.bandwidth() / 2)
+                        .y(d => y(d.best))
+
+                        // stop line if the value is null
+                        .defined(d => d.best);
+
+        console.log("-------------");
+        console.log("x:",  typeof x);
+        console.log("y:",  typeof y);
+        console.log("y1:", typeof y1);
+
+        console.log("-------------------------");
+        console.log("lineAvg:",  typeof lineAvg);
+        console.log("lineBest:", typeof lineBest);
+
+        switch(this._type) {
+            case "interval": {
+                this._drawChart(this._intvData, x, y, y1,
+                                lineBest, lineAvg); break; }
+            case "swing": {
+                this._drawChart(this._swngData, x, y, y1,
+                                lineBest, lineAvg); break; }
+            default:
+        }
+    };
+
+    _destroyChart = () => {
+
+    };
+
+    _updateChart = () => {
+
+    };
+
+    _drawChart = (data, x, y, y1, lineBest, lineAvg) => {
+        data = [...data];
+        console.log("-----------");
+        console.log("data:", data);
+
+        // add domains
+        x.domain(data.map(d => d.week));
+        y.domain([0, d3.max(data, d => d.best)]);
+        y1.domain([0, d3.max(data, d => d.score)]);
+
+        // adjust avg height
+        const rate1 = d3.max(data, d => d.score) /
+                      d3.max(data, d => d.best);
+
+        console.log("-------------");
+        console.log("rate1:", rate1);
+
+        // bar points
+        const grpElem  = d3.select(this._el.grp);
+        const barElems = grpElem.selectAll(this._classes.bar)
+                        .data(data);
+
+        console.log("-----------------");
+        console.log("grpElem:", grpElem);
+        console.log("barElems:", barElems);
+
+        barElems.enter()
+            .append("rect")
+            .attr("class", this._classes.bar)
+            .attr("x", (d) => x(d.week))
+            .attr("y", this._height)
+
+            .transition().duration(this._animation.bar.duration)
+            .delay((d, i) => (i * this._animation.bar.delay))
+
+            .attr("y", (d) => (y1(d.score)
+                            + (this._height - y1(d.score)
+                            - (this._height - y1(d.score)) * rate1)))
+
+            .attr("height", (d) => ((this._height - y1(d.score)) * rate1))
+            .attr("width", ((this._width / data.length) - (this._margin.bar /
+                            (this._windowWidth < this._mobileWidth.small ? 2 : 1))));
+
+        // line (best path)
+        grpElem.select(this._classes.path + this._modifiers.lineb).remove();
+
+        const pathElem = grpElem
+                        .append("path")
+                        .datum(data).attr("d", lineBest);
+
+        const elPath = pathElem.node();
+        elPath.classList.add(this._classes.path);
+        elPath.classList.add(this._classes.path + this._modifiers.lineb);
+
+        // path animation
+        const pathLength = pathElem.node().getTotalLength();
+
+        pathElem
+            .attr("stroke-dasharray",  pathLength + " " + pathLength)
+            .attr("stroke-dashoffset", pathLength)
+
+            .transition().duration(this._animation.path.duration)
+            .delay((d, i) => (i * this._animation.path.delay))
+            .attr("stroke-dashoffset", 0);
+
+        pathElem.exit().remove();
+    };
+
     // ---------------------------------------------
     //   Public methods
     // ---------------------------------------------
     // @name componentDidMount
     // @desc the function called after the component has mounted.
     componentDidMount() {
-        this._el = this.refs.el;
+        this._el.main = this.refs.el;
 
-        this._onWindowResize(this._el);
-
-        this._createChart(this._el);
+        this._onWindowResize();
+        this._createChart();
     }
 
     // @name componentWillUnmount
